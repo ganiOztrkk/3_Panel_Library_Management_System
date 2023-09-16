e6da7259ab46e9623ff88ca66aabfec0d4a1b594
Gani Ozturk <ganioztrk@gmail.com>
2023-09-16 22:31:25 +0300

    Initial Create

---
+++ LibraryProject/LibraryProject/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryProject.Models.Entity;
+
+namespace LibraryProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DbLibraryEntities _db = new DbLibraryEntities();
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Categories category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (category != null) _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var category = _db.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Categories category)
+        {
+            var updatedCategory = _db.Categories.Find(category.CategoryId);
+            if (updatedCategory != null) updatedCategory.CategoryName = category.CategoryName;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LibraryProject/LibraryProject/Controllers/WriterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryProject.Models.Entity;
+
+namespace LibraryProject.Controllers
+{
+    public class WriterController : Controller
+    {
+        private readonly DbLibraryEntities _libraryEntities = new DbLibraryEntities();
+        public ActionResult Index()
+        {
+            var writers = _libraryEntities.Writers.ToList();
+            return View(writers);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+    }
+}
