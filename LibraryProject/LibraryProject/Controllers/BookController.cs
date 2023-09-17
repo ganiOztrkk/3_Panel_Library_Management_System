0a5cc39a656e03d62476c5e985c5c37c050ddf49
Gani Ozturk <ganioztrk@gmail.com>
2023-09-17 03:04:59 +0300

    Category-Writer-Book-Search

---
+++ LibraryProject/LibraryProject/Controllers/BookController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryProject.Models.Entity;
+
+namespace LibraryProject.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly DbLibraryEntities _db = new DbLibraryEntities();
+        // GET: Book
+        public ActionResult Index(string search)
+        {
+            var books = _db.Books.ToList();
+            var filteredBooks = from i in _db.Books select i;
+            if (!string.IsNullOrEmpty(search))
+            {
+                filteredBooks = filteredBooks.Where(x => x.BookName.Contains(search));
+                return View(filteredBooks.ToList());
+            }
+            return View(books);
+        }
+
+        [HttpGet]
+        public ActionResult AddBook()
+        {
+            List<SelectListItem> categories = (from i in _db.Categories.ToList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = i.CategoryName,
+                                                    Value = i.CategoryId.ToString()
+                                                }).ToList();
+            ViewBag.Categories = categories;
+            List<SelectListItem> writers = (from k in _db.Writers.ToList()
+                                                select new SelectListItem()
+                                                {
+                                                    Text = k.WriterName + " " + k.WriterSurname,
+                                                    Value = k.WriterId.ToString()
+                                                }).ToList();
+            ViewBag.Writers = writers;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBook(Books book)
+        {
+            if (book != null) _db.Books.Add(book);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteBook(int id)
+        {
+            var book = _db.Books.Find(id);
+            if (book != null)
+            {
+                _db.Books.Remove(book);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateBook(int id)
+        {
+            var book = _db.Books.Find(id);
+            List<SelectListItem> categories = (from i in _db.Categories.ToList()
+                select new SelectListItem
+                {
+                    Text = i.CategoryName,
+                    Value = i.CategoryId.ToString()
+                }).ToList();
+                ViewBag.Categories = categories;
+
+                List<SelectListItem> writers = (from k in _db.Writers.ToList()
+                    select new SelectListItem()
+                    {
+                        Text = k.WriterName + " " + k.WriterSurname,
+                        Value = k.WriterId.ToString()
+                    }).ToList();
+                ViewBag.Writers = writers;
+            if (book != null)
+            {
+                return View(book);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult UpdateBook(Books books)
+        {
+            var updatedBook = _db.Books.Find(books.BookId);
+            if (updatedBook != null)
+            {
+                updatedBook.BookName = books.BookName;
+                updatedBook.Publisher = books.Publisher;
+                updatedBook.PublishYear = books.PublishYear;
+                updatedBook.Pages = books.Pages;
+                var category = _db.Categories.FirstOrDefault(x => x.CategoryId == books.Categories.CategoryId);
+                var writer = _db.Writers.FirstOrDefault(x => x.WriterId == books.Writers.WriterId);
+                if (category != null) updatedBook.BookCategory = category.CategoryId;
+                if (writer != null) updatedBook.BookWriter = writer.WriterId;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
--- LibraryProject/LibraryProject/Controllers/WriterController.cs
+++ LibraryProject/LibraryProject/Controllers/WriterController.cs
@@ -9,10 +9,10 @@ namespace LibraryProject.Controllers
 {
     public class WriterController : Controller
     {
-        private readonly DbLibraryEntities _libraryEntities = new DbLibraryEntities();
+        private readonly DbLibraryEntities _db = new DbLibraryEntities();
         public ActionResult Index()
         {
-            var writers = _libraryEntities.Writers.ToList();
+            var writers = _db.Writers.ToList();
             return View(writers);
         }
 
@@ -20,6 +20,42 @@ namespace LibraryProject.Controllers
         public ActionResult AddWriter()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writers writer)
+        {
+            _db.Writers.Add(writer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteWriter(int id)
+        {
+            var writer = _db.Writers.Find(id);
+            if (writer != null) _db.Writers.Remove(writer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateWriter(int id)
+        {
+            var writer = _db.Writers.Find(id);
+            return View(writer);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateWriter(Writers writer)
+        {
+            var updatedWriter = _db.Writers.Find(writer.WriterId);
+            if (updatedWriter != null)
+            {
+                updatedWriter.WriterName = writer.WriterName;
+                updatedWriter.WriterSurname = writer.WriterSurname;
+                updatedWriter.WriterDetail = writer.WriterDetail;
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
