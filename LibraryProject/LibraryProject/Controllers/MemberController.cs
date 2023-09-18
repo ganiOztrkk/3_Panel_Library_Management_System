cd98edd8e4391c4d76d19e697c25d7caea3b53ca
Gani Ozturk <ganioztrk@gmail.com>
2023-09-18 23:55:41 +0300

    PagedList

--- LibraryProject/LibraryProject/Controllers/MemberController.cs
+++ LibraryProject/LibraryProject/Controllers/MemberController.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LibraryProject.Models.Entity;
+using PagedList;
 
 namespace LibraryProject.Controllers
 {
     public class MemberController : Controller
     {
         private readonly DbLibraryEntities _db = new DbLibraryEntities();
-        public ActionResult Index()
+        public ActionResult Index(int page=1)
         {
-            var members = _db.Members.ToList();
+            var members = _db.Members.ToList().ToPagedList(page, 3);
             return View(members);
         }
 
@@ -41,7 +42,7 @@ namespace LibraryProject.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
-
+        [HttpGet]
         public ActionResult UpdateMember(int id)
         {
             var member = _db.Members.Find(id);
@@ -50,7 +51,20 @@ namespace LibraryProject.Controllers
         [HttpPost]
         public ActionResult UpdateMember(Members member)
         {
-            return View();
+            var updateMember = _db.Members.Find(member.MemberId);
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            updateMember.MemberName = member.MemberName;
+            updateMember.MemberSurname = member.MemberSurname;
+            updateMember.MemberPhone = member.MemberPhone;
+            updateMember.MemberSchool = member.MemberSchool;
+            updateMember.MemberImage = member.MemberImage;
+            updateMember.MemberUsername = member.MemberUsername;
+            updateMember.MemberPassword = member.MemberPassword;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
